162cee57703c0c0b9d2c79075df154d61b1e7a0e
Alexander-2049 <a.alexander.2049@gmail.com>
2024-07-10 22:15:37 +0300

    Added CRUD operations to user repository

--- backend/src/modules/users/users.repository.ts
+++ backend/src/modules/users/users.repository.ts
@@ -4,11 +4,48 @@ import { Prisma, User } from '@prisma/client';
 
 @Injectable()
 export class UsersRepository {
-  constructor(private prisma: PrismaService) {
-  }
-  
+  constructor(private prisma: PrismaService) {}
+
   async createUser(params: { data: Prisma.UserCreateInput }): Promise<User> {
     const { data } = params;
-    return this.prisma.user.create({data});
+    return this.prisma.user.create({ data });
+  }
+
+  async getUsers(params: {
+    cursor?: Prisma.UserWhereUniqueInput;
+    where?: Prisma.UserWhereInput;
+    orderBy?: Prisma.UserOrderByWithRelationInput;
+  }): Promise<User[]> {
+    const { cursor, where, orderBy } = params;
+    return this.prisma.user.findMany({
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async getUser(params: {
+    where?: Prisma.UserWhereUniqueInput;
+  }): Promise<User> {
+    const { where } = params;
+    return this.prisma.user.findUnique({
+      where,
+    });
+  }
+
+  async updateUser(params: {
+    where: Prisma.UserWhereUniqueInput;
+    data: Prisma.UserUpdateInput;
+  }): Promise<User> {
+    const { where, data } = params;
+    return this.prisma.user.update({ where, data });
+  }
+
+  async deleteUser(params: {
+    where: Prisma.UserWhereUniqueInput;
+  }): Promise<User> {
+    const { where } = params;
+    return this.prisma.user.delete({ where });
   }
 }
+
